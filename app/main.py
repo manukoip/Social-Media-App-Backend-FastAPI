980eafe993d6c6930daf0fd8080c86e5bf930f8e
manukoip <manukoipallil@yahoo.com>
2023-07-08 13:13:19 -0700

    new line didnt work from previous commit, revertin back

--- app/main.py
+++ app/main.py
@@ -30,4 +30,4 @@ app.include_router(vote.router)
 
 @app.get("/")
 def root():
-    return {"message": "Welcome to my API !!! \\n Add '/docs' to the end of the url to go to the API documentation"}
+    return {"message": "Welcome to my API !! -> Add '/docs' to the end of the url to go to the API documentation"}
