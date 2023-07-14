92c4bef15f3fd49de2bbbd382298c6dabecd8590
manukoip <manukoipallil@yahoo.com>
2023-07-14 13:50:34 -0700

    Added description to documentation

--- app/main.py
+++ app/main.py
@@ -6,10 +6,62 @@ from .routers import post, user, auth, vote
 from .config import settings
 
 # models.Base.metadata.create_all(bind=engine)
+description = """
+This is a simple Social Media Aplication. This API had **5 routes**:
 
-app = FastAPI()
+### Posts
+This route creates, reads, deletes and updates posts(CRUD)
 
-origins=["*"]
+
+### Users
+This route handles creating users and searching users by id
+
+
+### Authentication
+This route handles the user authentication for the applcation
+
+
+### Vote
+This route handles voting('like') and unvoting(remove the 'like') posts. This currently does not have a dislike functionality
+
+
+### Root
+This route is the default route  
+
+"""
+tags_metadata = [
+    {
+        "name": "Users",
+        "description": "Operations with users. Handles Creating Users and Searching users by id",
+    },
+    {
+        "name": "Posts",
+        "description": "Operations with posts. creates, reads, deletes and updates posts(CRUD)",
+    },
+    {
+        "name": "Authentication",
+        "description": "Operations with user authentication.",
+    },
+    {
+        "name": "Vote",
+        "description": "This route handles voting('like') and unvoting(remove the 'like') posts.",
+    },
+    {
+        "name": "Root",
+        "description": "The Default route",
+    },
+]
+app = FastAPI(
+    title="Social Media Application API",
+    description=description,
+    contact={
+        "name": ": Manu Mahadevan Koipalil",
+        "email": "manukoip20@gmail.com",
+    }, openapi_tags=tags_metadata,
+
+)
+
+origins = ["*"]
 
 app.add_middleware(
     CORSMiddleware,
@@ -28,6 +80,6 @@ app.include_router(vote.router)
 # Homepage for API
 
 
-@app.get("/")
+@app.get("/", tags=['Root'])
 def root():
-    return {"message": "Welcome to my API !! -> Add '/docs' to the end of the url to go to the API documentation"}
+    return {"message": "Welcome to my Social Media Application !! -> Add '/docs' to the end of the url to go to the API documentation"}
